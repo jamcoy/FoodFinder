f1d35d29929596db23c69658661a9873f537912f
jamcoy <james@coynemail.net>
2017-01-10 12:01:54 +0000

    Add placeholder controller functions

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -9,3 +9,19 @@ var sendJsonResponse = function(res, status, content) {
 module.exports.locationsCreate = function (req, res) {
     sendJsonResponse(res, 200, {"status" : "success"});
 };
+
+module.exports.locationsListByDistance = function (req, res) {
+    sendJsonResponse(res, 200, {"status" : "success"});
+};
+
+module.exports.locationsReadOne = function (req, res) {
+    sendJsonResponse(res, 200, {"status" : "success"});
+};
+
+module.exports.locationsUpdateOne = function (req, res) {
+    sendJsonResponse(res, 200, {"status" : "success"});
+};
+
+module.exports.locationsDeleteOne = function (req, res) {
+    sendJsonResponse(res, 200, {"status" : "success"});
+};
