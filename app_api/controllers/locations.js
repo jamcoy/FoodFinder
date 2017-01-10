fff0566125f45c7dac1b4eb82266a8e657a5f2a4
jamcoy <james@coynemail.net>
2017-01-10 14:30:57 +0000

    Handling of GET requests

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,22 +1,102 @@
 var mongoose = require('mongoose');
 var Loc = mongoose.model('Location');
 
-var sendJsonResponse = function(res, status, content) {
+var theEarth = (function(){
+    var earthRadius = 6371; // km
+    var getDistanceFromRads = function(rads) {
+        return parseFloat(rads * earthRadius);
+    };
+    var getRadsFromDistance = function(distance) {
+        return parseFloat(distance / earthRadius);
+    };
+    return {
+        getDistanceFromRads : getDistanceFromRads,
+        getRadsFromDistance : getRadsFromDistance
+    };
+})();
+
+
+var sendJSONresponse = function(res, status, content) {
     res.status(status);
     res.json(content);
 };
 
 module.exports.locationsCreate = function (req, res) {
-    sendJsonResponse(res, 200, {"status" : "success"});
+    sendJSONresponse(res, 200, {"status" : "success"});
 };
 
-module.exports.locationsListByDistance = function (req, res) {
-    sendJsonResponse(res, 200, {"status" : "success"});
+module.exports.locationsListByDistance = function(req, res) {
+    var lng = parseFloat(req.query.lng);
+    var lat = parseFloat(req.query.lat);
+    var maxDistance = parseFloat(req.query.maxDistance);
+    var point = {
+        type: "Point",
+        coordinates: [lng, lat]
+    };
+    var geoOptions = {
+        spherical: true,
+        maxDistance: theEarth.getRadsFromDistance(maxDistance),
+        num: 10
+    };
+    if (!lng || !lat || !maxDistance) {
+        console.log('locationsListByDistance missing params');
+        sendJSONresponse(res, 404, {
+            "message": "lng, lat and maxDistance query parameters are all required"
+        });
+        return;
+    }
+    Loc.geoNear(point, geoOptions, function(err, results, stats) {
+        var locations;
+        console.log('Geo Results', results);
+        console.log('Geo stats', stats);
+        if (err) {
+            console.log('geoNear error:', err);
+            sendJSONresponse(res, 404, err);
+        } else {
+            locations = buildLocationList(req, res, results, stats);
+            sendJSONresponse(res, 200, locations);
+        }
+    });
 };
 
-module.exports.locationsReadOne = function (req, res) {
-    sendJsonResponse(res, 200, {"status" : "success"});
+var buildLocationList = function(req, res, results, stats) {
+    var locations = [];
+    results.forEach(function(doc) {
+        locations.push({
+            distance: theEarth.getDistanceFromRads(doc.dis),
+            name: doc.obj.name,
+            address: doc.obj.address,
+            rating: doc.obj.rating,
+            facilities: doc.obj.facilities,
+            _id: doc.obj._id
+        });
+    });
+    return locations;
 };
+
+module.exports.locationsReadOne = function(req, res) {
+    if (req.params && req.params.locationid) {
+        Loc
+            .findById(req.params.locationid)
+            .exec(function(err, location) {
+                if (!location) {
+                    sendJsonResponse(res, 404, {
+                        "message": "locationid not found"
+                    });
+                    return;
+                } else if (err) {
+                    sendJsonResponse(res, 404, err);
+                    return;
+                }
+                sendJsonResponse(res, 200, location);
+            });
+    } else {
+        sendJsonResponse(res, 404, {
+            "message": "No locationid in request"
+        });
+    }
+};
+
 
 module.exports.locationsUpdateOne = function (req, res) {
     sendJsonResponse(res, 200, {"status" : "success"});
